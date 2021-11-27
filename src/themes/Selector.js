ebe49cd13df8a960ae326cf7a6950bca83b49713
Leander Seige <leander@seige.name>
2021-11-27 09:36:41 +0100

    sync

--- src/themes/Selector.js
+++ src/themes/Selector.js
@@ -1,4 +1,5 @@
-import Theme from './detektiiif2/Theme.js'
+// import Theme from './detektiiif2/Theme.js'
 // import Theme from './anotheruniversity/Theme.js'
+import Theme from './detektiiif2-external-theme-zb/Theme.js'
 
 export default Theme
