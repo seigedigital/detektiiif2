7a8fdbcf8e3a3efcfd8a7fe09f5428d8c3643987
Leander Seige <leander@seige.name>
2021-11-27 12:08:34 +0100

    basics

--- src/themes/_template/ThemeTemplate.js
+++ src/themes/_template/ThemeTemplate.js
@@ -1,6 +1,10 @@
 class ThemeTemplate {
   title = "DetektIIIF Version 2"
   logoImage = null
+  tabs = true
+  basket = true
+  showTabs = ['MANIFESTS','IMAGES','COLLECTIONS']
+  singleView = 'MANIFESTS'
 }
 
 export default ThemeTemplate
