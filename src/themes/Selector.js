189850e6ad665057d0b56f909af53e1073b931cc
Leander Seige <leander@seige.name>
2021-11-27 09:39:53 +0100

    sync

--- src/themes/Selector.js
+++ src/themes/Selector.js
@@ -1,5 +1,14 @@
-// import Theme from './detektiiif2/Theme.js'
+/*
+
+  In order to switch the build-time theme make sure that only one of the
+  following import statements is active!
+
+  More info on theming see README.md
+
+*/
+
+import Theme from './detektiiif2/Theme.js'
 // import Theme from './anotheruniversity/Theme.js'
-import Theme from './detektiiif2-external-theme-zb/Theme.js'
+// import Theme from './detektiiif2-external-theme-zb/Theme.js'
 
 export default Theme
